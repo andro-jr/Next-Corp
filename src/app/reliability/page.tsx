e2b040e1c25b3b430d7b165d26939d79704aaf67
andro-jr <prabin20panta@gmail.com>
2024-06-11 15:12:38 +0545

    route setup

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Reliabilitypage = () => {
+  return <div>Reliabilitypage</div>;
+};
+
+export default Reliabilitypage;
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScalePage = () => {
+  return <div>ScalePage</div>;
+};
+
+export default ScalePage;
