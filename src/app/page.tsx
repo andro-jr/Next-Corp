4baf9c1ec0cb3e56cbc9bdc3eaf609047ec33bff
andro-jr <prabin20panta@gmail.com>
2024-06-12 09:04:43 +0545

    component managemnet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="absolute -z-10 inset-0">
+      <Image
+        src="/home.jpg"
+        alt="Car factory"
+        fill
+        style={{ objectFit: "cover" }}
+      ></Image>
+    </div>
+  );
 }
